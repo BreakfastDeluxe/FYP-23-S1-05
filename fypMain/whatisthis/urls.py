812479505908ee3d59543cc7ceeeb7bccb3b9aca
Caroline <caroline.tangpq@outlook.com>
2023-03-09 22:47:05 +0800

    in progress

--- fypMain/whatisthis/urls.py
+++ fypMain/whatisthis/urls.py
@@ -20,6 +20,8 @@ from . import views
 from .views import * #import all functions from views.py
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     #path('', views.index, name='index'),
@@ -33,3 +35,4 @@ urlpatterns = [
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
