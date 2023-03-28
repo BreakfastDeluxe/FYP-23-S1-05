ba07679c5586fda5422cda88c0d808ef497894bd
BreakfastDeluxe <marcusmxk@gmail.com>
2023-03-28 18:19:26 +0800

    BugFix: upload image is now limited to 1MB in size

--- fypMain/whatisthis/models.py
+++ fypMain/whatisthis/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
-
+from .validators import *
 
 # Create your models here.
 
@@ -11,7 +11,7 @@ class Image(models.Model):
     #get the current logged in user's id and associate it with this uploaded image (set the ownership)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     #save the image to server
-    upload_Image = models.ImageField(upload_to='images/')
+    upload_Image = models.ImageField(upload_to='images/', validators=[validate_file_size])
     #setup blank field for storing caption generation later
     caption = models.CharField(max_length = 255, blank=True, null=True)
     #setup blank field for storing keyword generation later
